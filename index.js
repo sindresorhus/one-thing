193bb74c688fa7f2be57e41ce140cc025f911865
Sindre Sorhus <sindresorhus@gmail.com>
2023-06-27 01:55:04 +0200

    Fix getting text with Unicode
    Fixes #5

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ import childProcess from 'node:child_process';
 
 const execFile = promisify(childProcess.execFile);
 
+const decodeUnicodeEscapes = string => string.replace(/\\u([\da-f]{4})/gi, (_, p1) => String.fromCodePoint(Number.parseInt(p1, 16)));
+
 export default async function oneThing(text = '') {
 	await execFile('open', ['--background', `one-thing:?text=${encodeURIComponent(text)}`]);
 }
 
 export async function getOneThing() {
 	const {stdout} = await execFile('defaults', ['read', 'com.sindresorhus.One-Thing', 'text']);
-	return stdout.trim();
+	return decodeUnicodeEscapes(stdout.trim());
 }
